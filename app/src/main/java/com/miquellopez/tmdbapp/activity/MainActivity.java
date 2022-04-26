8474c776ca19e6c028e034a7826105db8bc039d8
Miquel <miloki27@outlook.com>
2022-04-26 16:32:16 +0200

    Añadir dependencias a librerias ButterKnife, Navigation y Retrofit

--- app/src/main/java/com/miquellopez/tmdbapp/activity/MainActivity.java
+++ app/src/main/java/com/miquellopez/tmdbapp/activity/MainActivity.java
@@ -1,8 +1,10 @@
-package com.miquellopez.tmdbapp;
+package com.miquellopez.tmdbapp.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.miquellopez.tmdbapp.R;
 
 public class MainActivity extends AppCompatActivity {
 
